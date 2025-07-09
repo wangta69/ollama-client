b8e9b89424ce978f2a1b207f4d7a779aa7047a77
pondol <y.h.ryu@tipszone.co.kr>
2025-07-09 15:38:21 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const axios = require('axios');
+const cheerio = require('cheerio'); // HTML 파싱용
+const mysql = require('mysql');
+
+async function fetchAndGenerateMeta(url) {
+  const html = await axios.get(url).then(res => res.data);
+  const $ = cheerio.load(html);
+
+  const content = [
+    $('title').text(),
+    $('meta[name="description"]').attr('content') || '',
+    $('h1').first().text(),
+    $('p').slice(0, 3).text()
+  ].join('\n');
+
+  const prompt = `
+다음은 웹 페이지 내용입니다.
+
+"${content}"
+
+위 내용을 바탕으로 다음을 생성해 주세요, 되도록 한글로 표현해줘:
+
+- title (30자 이내)
+- keywords (쉼표로 구분된 단어)
+- description (자연스럽고 명확한 100자 이내 문장)
+`;
+
+  const response = await axios.post('http://localhost:11434/api/generate', {
+    model: "mistral",
+    prompt,
+    stream: false
+  });
+
+  console.log("✅ 메타태그 결과:\n", response.data.response);
+}
+
+
+
+fetchAndGenerateMeta('https://www.onstory.fun/doc/programming/angular/http-interceptor')
